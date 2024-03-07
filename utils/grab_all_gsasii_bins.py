82236b81e16a6dd02a4187cc586aa464625f871b
Yuanpeng Zhang <zyroc1990@gmail.com>
2024-03-07 12:00:16 -0600

    add in grab binaries util

---
+++ utils/grab_all_gsasii_bins.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env /Users/y8z/opt/anaconda3/envs/g2python311/bin/python
+
+import urllib.request
+import tarfile
+import os
+
+thetarfile = "https://github.com/GSASII/binarytest/releases/download/v1.0.1/mac_64_p3.11_n1.26.tgz"
+os.chdir("/Users/y8z/Dev/pyGSAS/trunk/bindist")
+
+ftpstream = urllib.request.urlopen(thetarfile)
+thetarfile = tarfile.open(fileobj=ftpstream, mode="r|gz")
+thetarfile.extractall()
